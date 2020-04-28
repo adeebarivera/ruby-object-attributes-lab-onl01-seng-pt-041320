4126c4086841b9e13d03b4521b86996cd1af68c2
adeeba reaz <adeebarivera@outlook.com>
2020-04-28 20:16:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,21 @@
- Define ruby classes.
-# Define methods that read from and write to instance variables.
-# Create object properties using methods and instance variables.
+ class Dog
 
-# OVERVIEW
-
-# This lab is all about using instance variables within a class to create methods that represent attributes or properties of an object.
-
-# INSTRUCTIONS
-
-# DOG AND LIB/DOG.RB
-
-class Dog
-  
-  def name=(dogs_name)
-    @name = dogs_name
+  def name=(name)
+    @name = name
   end
-  
+
   def name
-    @name 
+    @name
   end
+
   def breed=(breed)
     @breed = breed
   end
-  
+
   def breed
     @breed
-  end 
-end
+  end
 
+
+end
+ 
